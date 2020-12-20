0fc0882942b1487507fd750c5cb5c2cb6ea053ad
Forrly <70975015+Forrly@users.noreply.github.com>
2020-12-20 14:51:21 +0300

    PracticalWork2

---
+++ Practice_2/src/laba2/TestAuthor.java
@@ -0,0 +1,8 @@
+package laba2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Joanne Rowling", "JR@juarow.ru", 'w');
+        System.out.println(author);
+    }
+}
