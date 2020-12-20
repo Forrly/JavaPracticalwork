63a2bc1493d6ef7faed768a52e8ea1c3f40583e0
Forrly <70975015+Forrly@users.noreply.github.com>
2020-12-20 14:48:21 +0300

    PracticalWork1

---
+++ Practice_1/src/laba1/TestBook.java
@@ -0,0 +1,9 @@
+package laba1;
+import java.lang.*;
+
+public class TestBook {
+    public static void main(String[] args) {
+        Book book = new Book("Harry Potter and the Philosopher's Stone", "Joanne Rowling",332,1000);
+        System.out.println(book);
+    }
+}
